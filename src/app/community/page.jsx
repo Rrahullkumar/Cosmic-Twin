71f298381ec5ee74668eed0f2c868e209ceac84d
Rrahullkumar <rahulkr811844@gmail.com>
2025-09-13 19:49:55 +0530

    build

--- src/app/community/page.jsx
+++ src/app/community/page.jsx
@@ -1,5 +1,8 @@
 import CommunityLayout from "../components/Community/CommunityLayout";
 
+export const dynamic = 'force-dynamic';
+
 export default function CommunityPage() {
+  
   return <CommunityLayout />;
 }
